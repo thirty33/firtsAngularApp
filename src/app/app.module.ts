6fd7914879a728078475640c3079f5ccf38ad951
Thirty33 <joelsuarez.1101@gmai.com>
2019-06-14 17:38:50 -0500

    managing components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-export class ProductListComponent {
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
     products = [
         {name: 'productOne', description: 'Product description', price: 900},
         {name: 'productOne', description: 'Product description', price: 900},
         {name: 'productOne', description: 'Product description', price: 900},
         {name: 'productOne', description: 'Product description', price: 900}
     ];
+
+    ngOnInit() {
+
+    }
 
     share() {
         window.alert('the product has been shared');
